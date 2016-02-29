59d9584c2e5336fc392ff0b27b0f5a711acc3c5e
MP Droog <mpdroog@icloud.com>
2016-02-29 16:13:37 +0100

    Feature. Write JSON to config.Output, Feature. Update JSON on error so errors don't get unnoticed

--- download/main.go
+++ download/main.go
@@ -22,6 +22,7 @@ type Config struct {
 	User    string
 	Pass    string
 	NzbDir  string
+	Output  string
 }
 
 type Perf struct {
@@ -29,7 +30,10 @@ type Perf struct {
 	Auth  float64
 	Arts  []float64
 	KBsec []float64
+	Error []string
 }
+
+var C Config
 
 func loadConfig(file string) (Config, error) {
 	var c Config
@@ -41,7 +45,27 @@ func loadConfig(file string) (Config, error) {
 	return c, e
 }
 
+func fail(e error) {
+	w, ew := os.Create(C.Output)
+	if ew != nil {
+		panic(ew)
+	}
+	defer w.Close()
+	enc := json.NewEncoder(w)
+
+	if ew = enc.Encode(Perf{
+		Arts: []float64{},
+		KBsec: []float64{},
+		Error: []string{e.Error()},
+	}); ew != nil {
+		panic(ew)
+	}
+
+	panic(e)
+}
+
 func main() {
+	var e error
 	var verbose, skipyenc bool
 	var configPath, date string
 	flag.BoolVar(&verbose, "v", false, "Verbosity")
@@ -50,51 +74,51 @@ func main() {
 	flag.StringVar(&date, "d", "", "YYYY-mm-dd to download from nzbdir")
 	flag.Parse()
 
-	c, e := loadConfig(configPath)
+	C, e = loadConfig(configPath)
 	if e != nil {
-		panic(e)
+		fail(e)
 	}
-	if !strings.HasSuffix(c.NzbDir, "/") {
-		c.NzbDir += "/"
+	if !strings.HasSuffix(C.NzbDir, "/") {
+		C.NzbDir += "/"
 	}
 	if date == "" {
 		// default to today
 		date = time.Now().Format("2006-01-02")
 	}
 	if verbose {
-		fmt.Printf("Config=%+v Date=%+v\n", c, date)
+		fmt.Printf("Config=%+v Date=%+v\n", C, date)
 	}
 
 	// Force valid date pattern
 	if _, e := time.Parse("2006-01-02", date); e != nil {
-		panic(e)
+		fail(e)
 	}
 
-	fd, e := os.Open(c.NzbDir + date + ".nzb")
+	fd, e := os.Open(C.NzbDir + date + ".nzb")
 	if e != nil {
-		panic(e)
+		fail(e)
 	}
 	defer fd.Close()
 	arts, e := nzb.Read(fd)
 	if e != nil {
-		panic(e)
+		fail(e)
 	}
 
 	if verbose {
 		fmt.Println("Connecting to nntp..")
 	}
-	conn := nntp.New(c.Address, "1", verbose)
+	conn := nntp.New(C.Address, "1", verbose)
 	conn.Verbose = verbose
 	perfBegin := time.Now()
 	var perfInit, perfAuth time.Time
 	{
 		defer conn.Close()
 		if e := conn.Init(); e != nil {
-			panic(e)
+			fail(e)
 		}
 		perfInit = time.Now()
-		if e := conn.Auth(c.User, c.Pass); e != nil {
-			panic(e)
+		if e := conn.Auth(C.User, C.Pass); e != nil {
+			fail(e)
 		}
 		perfAuth = time.Now()
 	}
@@ -112,11 +136,11 @@ func main() {
 
 		_, e = textproto.NewReader(rawread).ReadMIMEHeader()
 		if e != nil {
-			panic(e)
+			fail(e)
 		}
 
 		if _, e := io.Copy(buf, rawread); e != nil {
-			panic(e)
+			fail(e)
 		}
 		n := counter.ReadReset()
 		if int64(n) <= segment.Bytes {
@@ -125,7 +149,8 @@ func main() {
 
 		if !skipyenc {
 			if _, e = yenc.Decode(buf); e != nil {
-				panic(e)
+				fmt.Printf("%+v\n", string(buf.Bytes()))
+				fail(e)
 			}
 		}
 
@@ -145,16 +170,19 @@ func main() {
 		lastPerf = now
 	}
 
-	stat, e := json.Marshal(Perf{
+	w, e := os.Create(C.Output)
+	if e != nil {
+		fail(e)
+	}
+	defer w.Close()
+	enc := json.NewEncoder(w)
+
+	if e := enc.Encode(Perf{
 		Conn:  MilliSeconds(perfInit.Sub(perfBegin)),
 		Auth:  MilliSeconds(perfAuth.Sub(perfInit)),
 		Arts:  perfArts,
 		KBsec: KBsecs,
-	})
-	if e != nil {
-		panic(e)
-	}
-	if _, e := os.Stdout.Write(stat); e != nil {
-		panic(e)
+	}); e != nil {
+		fail(e)
 	}
 }
